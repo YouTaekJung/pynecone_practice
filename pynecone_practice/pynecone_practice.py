41a9664b53587c65923240419a7e8552cf25236d
youtaekjung <jyt9319@gmail.com>
2023-01-31 16:50:21 +0900

    Add sample code

--- pynecone_practice/pynecone_practice.py
+++ pynecone_practice/pynecone_practice.py
@@ -8,30 +8,30 @@ filename = f"{config.app_name}/{config.app_name}.py"
 
 
 class State(pc.State):
-    """The app state."""
+    count: int = 0
 
-    pass
+    def increment(self):
+        self.count += 1
+
+    def decrement(self):
+        self.count -= 1
 
 
 def index():
-    return pc.center(
-        pc.vstack(
-            pc.heading("Welcome to Pynecone!", font_size="2em"),
-            pc.box("Get started by editing ", pc.code(filename, font_size="1em")),
-            pc.link(
-                "Check out our docs!",
-                href=docs_url,
-                border="0.1em solid",
-                padding="0.5em",
-                border_radius="0.5em",
-                _hover={
-                    "color": "rgb(107,99,246)",
-                },
-            ),
-            spacing="1.5em",
-            font_size="2em",
+    return pc.hstack(
+        pc.button(
+            "Decrement",
+            color_scheme="red",
+            border_radius="1em",
+            on_click=State.decrement,
         ),
-        padding_top="10%",
+        pc.heading(State.count, font_size="2em"),
+        pc.button(
+            "Increment",
+            color_scheme="green",
+            border_radius="1em",
+            on_click=State.increment,
+        ),
     )
 
 
